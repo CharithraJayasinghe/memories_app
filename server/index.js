68a427d17fa6dc79334239261c5e178bafe7323d
Charithra Jayasinghe <charithraseneth3@gmail.com>
2023-01-18 23:19:30 +0530

    first commit

--- server/index.js
+++ server/index.js
@@ -9,11 +9,24 @@ app.use(bodyParser.json({ limit: "30mb" , extended: true }))
 app.use(bodyParser.urlencoded({ limit: "30mb" , extended: true }))
 app.use(cors());
 
-const CONNECTION_URL = 'mongodb+srv://Charithra:charithra98@cluster0.rt5htvp.mongodb.net/?retryWrites=true&w=majority'
-const PORT = process.env.PORT || 5000;
+mongoose.connect('mongodb+srv://Charithra:charithra98@cluster0.rt5htvp.mongodb.net/test1?retryWrites=true&w=majority', ()=>{
+  console.log('database connection established');
+})
 
-mongoose.connect(CONNECTION_URL, {useNewUrlParser: true, useUnifiedTopology: true})
-  .then(() => app.listen(PORT, console.log('Server running on port: ${PORT}')))
-  .catch((error) => console.log(error.massage));
+/*const CONNECTION_URL = 'mongodb+srv://Charithra:charithra98@cluster0.rt5htvp.mongodb.net/?retryWrites=true&w=majority'
+/*const PORT = process.env.PORT || 5000;
 
- mongoose.set('useFindAndModify', false); 
+mongoose.connect(CONNECTION_URL, {
+  useNewUrlParser: true, 
+  useUnifiedTopology: true});
+ /* .then(() => app.listen(PORT, console.log('Server running on port: ${PORT}')))
+  .catch((error) => console.log(error.massage));*/
+
+ /*mongoose.set('useFindAndModify', false); 
+
+ app.get('/', (req, res) => {
+  res.send('hello world')
+ }
+ )
+*/
+app.listen(5000,(console.log('Server running on port: 5000')))
